c4b35e1fb3a69bb83aa360cb2d2985cf14c0aa84
Saif <droconos99@gmail.com>
2024-02-21 17:31:46 +0600

    added fetch() for login successfully

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, session
+from flask_cors import CORS
 from controller.user_controller import *
 from controller.flight_controller import *
 from controller.booking_controller import *
@@ -8,6 +9,7 @@ from routes.booking_route import booking_bp
 
 app = Flask(__name__)
 app.secret_key = "airline"
+CORS(app)
 
 #to do, remove all the single quotes with double quotes
 app.register_blueprint(user_bp)
--- frontend/scripts/auth.js
+++ frontend/scripts/auth.js
@@ -1,10 +1,62 @@
-const loginForm = document.getElementById("login-form");
-const loginButton = document.getElementById("login-form-submit");
-const signupForm = document.getElementById("signup-form");
-const signupButton = document.getElementById("signup-form-submit");
+document.addEventListener("DOMContentLoaded", function () {
+   const loginForm = document.getElementById("login-form");
 
-loginButton.addEventListener("click", (e) => {
-   e.preventDefault();
-   const username = loginForm.username.value;
-   const password = loginForm.password.value;
+   loginForm.addEventListener("submit", function (event) {
+      event.preventDefault();
+
+      const email = document.getElementById("email").value;
+      const password = document.getElementById("password").value;
+      const userType = document.querySelector('input[name="user"]:checked').value;
+
+      const loginData = {
+         email: email,
+         password: password,
+      };
+
+      console.log(JSON.stringify(loginData));
+
+      if (userType === "customer") {
+         fetch("http://127.0.0.1:5000/api/v1/login", {
+            method: "POST",
+            headers: {
+               "Content-Type": "application/json",
+            },
+            body: JSON.stringify(loginData),
+         })
+            .then((response) => {
+               if (!response.ok) {
+                  throw new Error("Network response was not ok");
+               }
+               window.location.href = "customer.html";
+               return response.json();
+            })
+            .then((data) => {
+               console.log(data);
+            })
+            .catch((error) => {
+               console.log("There was a problem with your fetch operation:", error);
+            });
+      } else {
+         fetch("http://127.0.0.1:5000/api/v1/login/admin", {
+            method: "POST",
+            headers: {
+               "Content-Type": "application/json",
+            },
+            body: JSON.stringify(loginData),
+         })
+            .then((response) => {
+               if (!response.ok) {
+                  throw new Error("Network response was not ok");
+               }
+               window.location.href = "admin.html";
+               return response.json();
+            })
+            .then((data) => {
+               console.log(data);
+            })
+            .catch((error) => {
+               console.log("There was a problem with your fetch operation:", error);
+            });
+      }
+   });
 });
